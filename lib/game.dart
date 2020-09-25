d288e820fa3e39e5cc2324afd8c7740b42d7243b
SamMalcolm <sam_malcolm@live.com.au>
2020-09-25 14:56:25 +1000

    sidebar score lines done

--- lib/game.dart
+++ lib/game.dart
@@ -13,34 +13,86 @@ class Game extends StatefulWidget {
 }
 
 class _Game extends State<Game> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Snooker Scoreboard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          RaisedButton(
-            child: Text('Return'),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        MyHomePage(title: 'Snooker Scoreboard'),
-                  ));
-            },
+  Widget returnScoreLine() {
+    return Container(
+      height: double.infinity,
+      width: 35.00,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          FractionallySizedBox(
+            widthFactor: 1,
+            heightFactor: 1,
+            child: Container(
+                color: Color(0xFFCCCCCC),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 36.0),
+                  child: Text('129',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black)),
+                )),
           ),
-          Text(widget.playerNames[0]),
-          if (widget.playerHandicaps != null)
-            Text('${widget.playerHandicaps[0]}'),
-          Text(widget.playerNames[1]),
-          if (widget.playerHandicaps != null)
-            Text('${widget.playerHandicaps[1]}'),
+          FractionallySizedBox(
+            widthFactor: 1,
+            heightFactor: 0.5,
+            child: Container(
+                color: Color(0xFFC3A164),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text('129',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white)),
+                )),
+          ),
+          FractionallySizedBox(
+            widthFactor: 1,
+            heightFactor: 0.2,
+            child: Container(
+                color: Color(0xFF7D73B5),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text('129',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white)),
+                )),
+          ),
         ],
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      // crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        returnScoreLine(),
+        Column(
+          children: [
+            RaisedButton(
+              child: Text('Return'),
+              onPressed: () {
+                Navigator.pop(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          MyHomePage(title: 'Snooker Scoreboard'),
+                    ));
+              },
+            ),
+            Text(widget.playerNames[0]),
+            if (widget.playerHandicaps != null)
+              Text('${widget.playerHandicaps[0]}'),
+            Text(widget.playerNames[1]),
+            if (widget.playerHandicaps != null)
+              Text('${widget.playerHandicaps[1]}'),
+          ],
+        ),
+        returnScoreLine(),
+      ],
+    ));
   }
 }
