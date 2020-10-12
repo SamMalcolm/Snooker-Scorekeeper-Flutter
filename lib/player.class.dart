41090803e2274ffaf58f332842a261e1e9567623
SamMalcolm <sam_malcolm@live.com.au>
2020-10-12 19:18:25 +1100

    its working

--- lib/player.class.dart
+++ lib/player.class.dart
@@ -46,19 +46,24 @@ class Player {
 
     int scoringPoints = 0;
     snookersReqdFractionOfMax = 0;
-    if (pointsRemaining == 7) {
-      if (opponentsScore < score) {
-        winningScorelineColor = const Color(0xff0B0B0B);
-        snookersReqdScoreline = maxScore;
-        snookersReqdFractionOfMax = 1.0;
-      }
+    int diff = (opponentsScore > score)
+        ? (opponentsScore - score)
+        : (score - opponentsScore);
+
+    if (diff <= 7 && pointsRemaining == 7) {
+      snookersReqdFractionOfMax = 1.0;
+      snookersReqdScoreline = maxScore;
+      winningScorelineColor = const Color(0xff0B0B0B);
     } else {
       while (scoringPoints <= pointsRemaining) {
         int pottentialScore = score;
         int pottentialPointsRemaining = pointsRemaining;
         pottentialScore += scoringPoints;
         pottentialPointsRemaining -= scoringPoints;
-
+        print("POTTENTIAL SCORE");
+        print(pottentialScore);
+        print("POTTENTIAL POINTS REMAINING");
+        print(pottentialPointsRemaining);
         if (opponentsScore + pottentialPointsRemaining < pottentialScore) {
           snookersReqdScoreline = pottentialScore;
           break;
@@ -93,45 +98,13 @@ class Player {
           break;
         }
       }
-      snookersReqdFractionOfMax = snookersReqdScoreline / maxScore;
-
-      if (snookersReqdScoreline > maxScore ||
-          opponentsScore + pointsRemaining < score) {
-        snookersReqdFractionOfMax = 0;
-      }
     }
+    snookersReqdFractionOfMax = snookersReqdScoreline / maxScore;
 
-    //   while (scoringPoints <= pointsRemaining && pointsRemaining <= 27) {
-    //     int pottentialScore = score;
-    //     int pottentialPointsRemaining = pointsRemaining;
-
-    //     if (pottentialPointsRemaining == 27) {
-    //       scoringPoints += 2;
-    //       winningScorelineColor = const Color(0xffCEB636);
-    //     } else if (pottentialPointsRemaining == 25) {
-    //       scoringPoints += 3;
-    //       winningScorelineColor = const Color(0xff397140);
-    //     } else if (pottentialPointsRemaining == 22) {
-    //       scoringPoints += 4;
-    //       winningScorelineColor = const Color(0xff694A20);
-    //     } else if (pottentialPointsRemaining == 18) {
-    //       scoringPoints += 5;
-    //       winningScorelineColor = const Color(0xff2F4EB4);
-    //     } else if (pottentialPointsRemaining == 13) {
-    //       scoringPoints += 6;
-    //       winningScorelineColor = const Color(0xff9B3D9B);
-    //     } else if (pottentialPointsRemaining == 7) {
-    //       scoringPoints += 7;
-    //       winningScorelineColor = const Color(0xff0B0B0B);
-    //     }
-
-    //     pottentialScore += scoringPoints;
-    //     pottentialPointsRemaining -= scoringPoints;
-    //     if (opponentsScore + pottentialPointsRemaining < pottentialScore) {
-    //       snookersReqdScoreline = pottentialScore;
-    //       break;
-    //     }
-    //   }
+    if (snookersReqdScoreline > maxScore ||
+        opponentsScore + pointsRemaining < score) {
+      snookersReqdFractionOfMax = 0;
+    }
   }
 
   Player(this.name, this.handicap) {
